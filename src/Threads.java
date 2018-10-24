84e857ce2a85f7daacf5824c1688d6bceeab118a
nabiharaza <nabiha_137@hotmail.com>
2018-10-23 21:36:19 -0400

    Testing:
    Joining

--- src/Threads.java
+++ src/Threads.java
@@ -5,19 +5,35 @@ class Threads {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("One");
-                count++;
+                for (int i = 0; i <= 100; i++) {
+                    System.out.println("One");
+                    count++;
+                }
+
             }
         });
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
                 System.out.println("TWO");
+                for (int i = 0; i <= 10; i++) {
+                    System.out.println("Two");
+                    count++;
+                }
                 count++;
             }
         });
         t1.start();
         t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
         System.out.println(count);
     }
 }
