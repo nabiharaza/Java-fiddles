a7b07b82d87ebc98eedaf73681d0d99db6c172a2
nabiharaza <nabiha_137@hotmail.com>
2018-12-02 16:21:12 -0500

    RMI Simple

--- RMI/src/Server.java
+++ RMI/src/Server.java
@@ -1,11 +1,14 @@
 
 import java.net.MalformedURLException;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Server {
     public static void main(String[] args) throws RemoteException, MalformedURLException {
         AddC obj = new AddC ();
-        Naming.rebind ( "ADD",obj );
+        Registry registry = LocateRegistry.createRegistry ( 2211 );
+        registry.rebind ( "ADD",obj );
         System.out.println ("Server Started" );
     }
 }
