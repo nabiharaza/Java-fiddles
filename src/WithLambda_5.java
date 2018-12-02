214977090f4321326416dbf3af8461bfa55fc893
nabiharaza <nabiha_137@hotmail.com>
2018-12-02 14:40:42 -0500

    Lambda expressions

--- src/WithLambda_5.java
+++ src/WithLambda_5.java
@@ -1,21 +1,23 @@
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+        import java.util.Comparator;
+        import java.util.Collections;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.stream.Collectors;
 
 public class WithLambda_5 implements Check {
 
+
     public boolean doTheCheck(Id p, int low, int high)  {
-		return p.getNumber() >= low  && p.getNumber() <= high;
+        return p.getNumber() >= low  && p.getNumber() <= high;
     }
     public static void printPersons(List<Id> roster, Check tester, int low, int high) {
-	    for (Id p : roster) {
-		if (tester.doTheCheck(p, low, high))
-		    p.printName();
-	    }
+        for (Id p : roster) {
+            if (tester.doTheCheck ( p, low, high ))
+                p.printName();
+        }
     }
+
 
     public void work()	{
         List<Id> persons = Arrays.asList(
@@ -23,9 +25,12 @@ public class WithLambda_5 implements Check {
                 new Id("Wolfgang",	123),
                 new Id("Thomas",	12) );
 
-	printPersons(persons, (Id p, int low, int high) -> p.getNumber() >= low && p.getNumber() <= high, 12, 123 );
+//        Check c1 = (p, low, high)->p.getNumber() >= low  && p.getNumber() <= high;
+        printPersons(persons, (p, low, high)->p.getNumber() >= low  && p.getNumber() <= high, 10  ,10);
     }
     public static void main(String[] args) {
-	new WithLambda_5().work();
+
+
+        new WithLambda_5().work();
     }
 }
