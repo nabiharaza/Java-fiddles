fe22654052e03e0e609fc8a4acc60a20f7861b23
nabiharaza <nabiha_137@hotmail.com>
2018-12-15 15:18:54 -0500

    Networking practice

---
+++ src/Networks/HostInfo.java
@@ -0,0 +1,18 @@
+package Networks;
+
+
+		import java.net.*;
+		import java.io.*;
+		import java.util.*;
+
+public class HostInfo {
+	public static void main(String argv[]) {
+		InetAddress ipAddr;
+		try {
+			ipAddr = InetAddress.getLocalHost();
+			System.out.println ("This is "+ipAddr);
+		} catch (UnknownHostException e) {
+			System.out.println ("Unknown host");
+		}
+	}
+}
