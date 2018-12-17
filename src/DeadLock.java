7f4433d03834e11af111001cf5c0c039fd081a93
nabiharaza <nabiha_137@hotmail.com>
2018-12-16 23:14:00 -0500

    Dinning Philsopers

--- src/DeadLock.java
+++ src/DeadLock.java
@@ -1,39 +1,50 @@
-import java.util.*;
+public class DeadLock extends Thread {
+    private static String o1 = new String ( );
+    private static String o2 = new String ( );
+    private String info;
 
-public class DeadLock extends Thread	{
-	private static String o1  = new String();
-	private static String o2  = new String();
-	private String info;
+    public DeadLock(String info) {
+        this.info = info;
+    }
 
-	public DeadLock (String info) {
-		this.info    = info;
-	}
+    private void inProtected_1() throws InterruptedException {
+        synchronized (o2) {
+            System.out.println ( "In function 1 " + Thread.currentThread ( ).getName ( ) );
+            inProtected_2 ( );
+        }
+    }
 
-	private void inProtected_1 () {
-		synchronized ( o2 )   {
-			inProtected_2();
-		}
-	}
+    private void inProtected_2() throws InterruptedException {
+        sleep ( 1000 );
+        synchronized (o1) {
+            System.out.println ( "In function 2 " + Thread.currentThread ( ).getName ( ) );
+            inProtected_1 ( );
+        }
+    }
 
-	private void inProtected_2 () {
-		synchronized ( o1 )   {
-			inProtected_1();
-		}
-	}
+    public void run() {
+        if (info.equals ( "first" )) {
+            synchronized (o1) {
+                System.out.println ( "In run 1" + Thread.currentThread ( ).getName ( ) );
+                try {
+                    inProtected_1 ( );
+                } catch (InterruptedException e) {
+                    e.printStackTrace ( );
+                }
+            }
+        } else
+            synchronized (o2) {
+                System.out.println ( "In run 2" + Thread.currentThread ( ).getName ( ) );
+                try {
+                    inProtected_2 ( );
+                } catch (InterruptedException e) {
+                    e.printStackTrace ( );
+                }
+            }
+    }
 
-	public void run () {
-		if ( info.equals("first") )	{
-			synchronized ( o1 )   {
-				inProtected_1();
-			}
-		} else
-			synchronized ( o2 )   {
-				inProtected_2();
-			}
-	}
-
-	public static void main (String args []) {
-		new DeadLock("second").start();
-		new DeadLock("first").start();
-	}
+    public static void main(String args[]) {
+        new DeadLock ( "second" ).start ( );
+        new DeadLock ( "first" ).start ( );
+    }
 }
---
+++ src/Networks/Philosopher.java
@@ -0,0 +1,50 @@
+package Networks;
+
+import java.util.Random;
+import java.util.Vector;
+
+/** A class implementing the Dining Philosphers */
+public class Philosopher extends Thread {
+
+  protected static Random random = new Random();        // randomize
+  protected int me;             	// number for trace
+  protected Integer left, right;   	// my chopsticks
+
+  public Philosopher (int me, Integer left, Integer right) {
+    this.me = me; this.left = left; this.right = right;
+  }
+/** philosopher's body: think and eat 5 times */
+  public void run () {
+    for (int n = 1; n <= 5; ++ n) {
+      System.out.println(me+" thinks");
+      try {
+        Thread.sleep((long)(random.nextFloat()*1000));
+      } catch(Exception e) {
+	e.printStackTrace();
+      }
+      System.out.println(me+" is trying to eat"); 
+      synchronized ( left )  {
+	   synchronized ( right )  {
+		    System.out.println("\t" + me+" eats"); 
+		    try {
+		      Thread.sleep((long)(random.nextFloat()*1000));
+                    } catch(Exception e) {
+			e.printStackTrace();
+	            }
+	  }
+    }
+    System.out.println("\t" + me+" leaves");
+  }
+ }
+/** sets up for 5 philosophers */
+  public static void main (String args []) {
+    Integer f[] = new Integer[5];
+    for (int n = 0; n < 5; ++ n)
+		f[n] = new Integer(n);
+    Philosopher p[] = new Philosopher[5];
+    p[0] = new Philosopher(0, f[4], f[0]);      // backwards
+    for (int n = 1; n < 5; ++ n)
+		p[n] = new Philosopher(n, f[n-1], f[n]);
+    for (int n = 0; n < 5; ++ n) p[n].start();
+  }
+}
